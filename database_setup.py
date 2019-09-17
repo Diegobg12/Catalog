031543d332c332ffd8b6a707845119ef71c10566
Diego Bustos <diegobustos1229@gmail.com>
2019-09-17 12:09:54 -0500

    Create time column

--- database_setup.py
+++ database_setup.py
@@ -1,13 +1,19 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+
 Base = declarative_base()
 
 # Users declaration
+
+def _get_date():
+    return datetime.datetime.now()
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -21,7 +27,7 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
+    
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -38,6 +44,7 @@ class CatItem(Base):
     description = Column(String(250))
     cat_name = Column(ForeignKey('category.name'))
     category = relationship(Category)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
 
     @property
     def serialize(self):
