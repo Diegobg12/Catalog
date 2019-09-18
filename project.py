89c0fb7681670febe3dd7f488513f1fbd42e302e
Diego Bustos <diegobustos1229@gmail.com>
2019-09-18 07:42:46 -0500

    View Item Descriptions

--- project.py
+++ project.py
@@ -30,14 +30,14 @@ def showItems(cat_name):
     Items = session.query(CatItem).filter_by(cat_name = Cat.name)
     return render_template('items.html', Categories = Categories, Items = Items, Cat = Cat)
 
-# @app.route('/catalog/<cat_name>/<item_name>')
-# def ItemDescription(cat_name):
-#     # return "This Page will show all categories and the last item added"
-#     DBSession = sessionmaker(bind=engine)
-#     session = DBSession()
-#     Cat = session.query(Category).filter_by(name = cat_name).one()
-#     Items = session.query(CatItem).filter_by(cat_name = Cat.name)
-#     return render_template('items.html', Cat = Cat, Items = Items, cat_name= cat_name)
+@app.route('/catalog/<cat_name>/<item_name>')
+def ItemDescription(cat_name, item_name):
+    # return "This Page will show all categories and the last item added"
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    Cat = session.query(Category).filter_by(name = cat_name).one()
+    Item = session.query(CatItem).filter_by(name = item_name, cat_name = cat_name).one()
+    return render_template('description.html', Cat = Cat, Item = Item, cat_name = cat_name)
 
 @app.route('/catalog/<cat_name>/new', methods=['GET', 'POST'])
 def createItem(cat_name):
